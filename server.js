f2b753bee2c55abff829a07df5992a00446d9272
xdemocle <xdemocle@gmail.com>
2018-01-11 15:00:02 +0200

    change package with new main script; change localhost

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const remoteData = require('./backend/remote-data');
 
 // Create a server with a host and port
 const server = Hapi.server({
-  host: 'localhost',
+  host: '0.0.0.0',
   port: process.env.PORT || 5000,
   routes: {
     cors: true
