c4fd1841fd60f27d52c8a5aba3a297bc3388653d
xdemocle <xdemocle@gmail.com>
2018-02-14 10:18:45 +0200

    adding 3 weeks stats

--- backend/config.js
+++ backend/config.js
@@ -14,7 +14,7 @@ const collections = {
 
 const coinmarketcapUrl = 'https://api.coinmarketcap.com/v1/global/';
 
-const timing = [5, 15, 30, 60, 180, 360, 540, 720, 1080, 1440, 2880, 4320, 10080, 20160, 40320];
+const timing = [5, 15, 30, 60, 180, 360, 540, 720, 1080, 1440, 2880, 4320, 10080, 20160, 30240, 40320];
 
 module.exports = {
   dbOpts,
